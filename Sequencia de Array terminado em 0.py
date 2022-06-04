0c4d0c8a89287bf01727c696344302826c2d14b9
Anderson Moreira <105467562+andmorx@users.noreply.github.com>
2022-06-04 18:18:30 -0300

    Add files via upload
    Função Fatorial, Função de dois coeficientes, Sequencia de Array terminado em 0, Inversão de impressão ao digitar 0, Função de Baskara e Números Inteiros com Repetição Encaixada

---
+++ Sequencia de Array terminado em 0.py
@@ -0,0 +1,13 @@
+seq = []
+i = 1
+
+while i > 0:
+    n = int(input("Digite n: "))    
+    seq.append(n)
+
+    while n == 0:
+
+        for i in seq [i]:
+
+            print (i)
+            i -= 0
